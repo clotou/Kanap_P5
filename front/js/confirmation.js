7000601eefc5055013d38f8b29a4c517f99b0b3f
clotilde lacaille <lacailleclotilde@gmail.com>
2022-06-14 09:50:44 +0200

    effacage du local storage après confrmation de commande

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -4,3 +4,4 @@ const id = new URL(window.location.href).searchParams.get("id");
 //on insere l'id de la commande dans la page
 let printOrderId = document.getElementById('orderId');
 printOrderId.textContent = id;
+localStorage.clear();
